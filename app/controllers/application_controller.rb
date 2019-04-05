83b5bf30bac46f7bd980e9964fac7d1f33752f8f
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2019-04-05 04:55:22 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,6 +28,10 @@ class ApplicationController < Sinatra::Base
      erb :show
   end
   
-  post ""
+  get "/recipes/:id/edit" do
+    @recipe = Recipe.find(params[:id])
+
+     erb :edit
+  end
 
 end
